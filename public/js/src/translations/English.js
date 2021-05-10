c3146327c98f18c9a925e7b629db22e122d09257
Adam Jonas <adamjonas25@gmail.com>
2021-05-10 13:12:56 +0100

    Add error message constants to translations file

---
+++ public/js/src/translations/English.js
@@ -0,0 +1,10 @@
+const GAME_OVER_ERROR = 'Cannot add roll when the game is over'
+const GAME_OVER_ALERT = 'This game is over! Refresh to score a new game'
+const INVALID_INPUT_ERROR = [
+  'Invalid roll - must be a positive number between ',
+  '0 and the remaining pins of the frame'
+].join('')
+const INVALID_INPUT_ALERT = [
+  'Please enter a valid roll! Must be a number ',
+  'between 0 and the remaining pins of the frame'
+].join('')
